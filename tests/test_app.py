d6477f6c6bd5ea07286a51bbccf3a9037f88013c
Yuri Tayro <96271669+YuriTayro@users.noreply.github.com>
2025-09-19 11:38:09 -0300

    Adicionando schema de mensagem.

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,13 +6,14 @@ from fastapi_zero.app import app
 
 
 def test_root_deve_retornar_hello_world():
-    '''
+    """
     Esse teste tem três etapas (AAA):
-    1. Arrange (preparar): configurar o ambiente e os dados necessários para o teste.
+    1. Arrange (preparar): configurar o ambiente e os dados necessários para
+    o teste.
     2. Act (agir): executar a funcionalidade que está sendo testada.
     3. Assert (afirmar): verificar se o resultado obtido é o esperado.
-    '''
+    """
     client = TestClient(app)
-    response = client.get('/')
+    response = client.get('/') #captura a resposta da requisição
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'Hello': 'World'}
+    assert response.json() == {'message': 'World'}
