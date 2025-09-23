33e4638c6e8e3b97b66871aa7b502a3d43aa1fb2
yuri.tayrocnp <yuri.tayrocnp@gmail.com>
2025-09-22 22:29:57 -0300

    algumas alterações nos arquivos de teste.

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,14 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import select
+
 from fastapi_zero.models import User
 
 
 def test_create_user(session):
-    # vamos passar os campos obrigatórios
-    user = User(username='testuser', email='test@test', password='1234')
-
-    session.add(user)
-    # usa o commit para q as iinformações criadas na sessao seja trasnmitida
-    # para o banco de dados
+    new_user = User(username='testuser', email='test@test', password='1234')
+    session.add(new_user)
     session.commit()
-    breakpoint()  # ponto de interrupção para depuração
 
+    user = session.scalar(select(User).where(User.username == 'testuser'))
+
+    # Verifica se os dados fixos estão corretos
+    assert user.id is not None
     assert user.username == 'testuser'
+
+    # A verificação importante:
+    # Apenas garante que um objeto datetime foi criado
+    assert isinstance(user.created_at, datetime)
